3f8b748378a0afb9481879101204d6c623b8ba21
ndpm <ngoduyphuc820@gmail.com>
2022-02-28 22:35:31 +0700

    fix index.js

--- index.js
+++ index.js
@@ -6,8 +6,5 @@ const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-    var a = 1;
-    var b = 2;
-    var c = a + b;
 });
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
